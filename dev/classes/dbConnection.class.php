5ff223ad2cac88cb1f2abbf9ccfaf9849cd9f9c6
a1306537 <a1306536@unet.univie.ac.at>
2015-06-08 15:36:32 +0200

    Benutzer anlegen funktioniert Profilbild auf den Server hochladen funkt auch

--- dev/classes/dbConnection.class.php
+++ dev/classes/dbConnection.class.php
@@ -3,9 +3,10 @@
 	$servername = "localhost";
 	$username = "";
 	$password = "";
+	$database = "test";
 	
 	// erzeugt die DB-Verbindung
-	$conn = new mysqli($servername, $username, $password);
+	$conn = new mysqli($servername, $username, $password, $database);
 	
 	if ($conn->connect_error) {
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
