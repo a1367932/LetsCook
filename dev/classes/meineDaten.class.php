e1d3f72b1dc5b54f162e033894b3239f4b8480ca
a1306537 <a1306536@unet.univie.ac.at>
2015-06-08 16:37:01 +0200

    Benutzerdaten können geändert werden

--- dev/classes/meineDaten.class.php
+++ dev/classes/meineDaten.class.php
@@ -11,18 +11,34 @@ if ( !isset( $_POST['meineDatenSubmit'] ) ) {
 	$smarty->setCacheDir('../Smarty/cache');
 	$smarty->setConfigDir('../Smarty/configs');
 
-	$smarty->assign('vorname', 'Max');
-	$smarty->assign('nachname', 'Mustermann');
-	$smarty->assign('strasse', 'Musterstrasse');
-	$smarty->assign('nummer', 'Musternummer');
-	$smarty->assign('plz', 'Musterplz');
-	$smarty->assign('ort', 'Musterort');
-	$smarty->assign('land', 'Musterland');
-	$smarty->assign('email', 'muster@muster.at');
-	$smarty->assign('alter', 'aktuelesDatum-geburtsdatum');
-	$smarty->assign('bbild', 'pfad zum bild');
-	$smarty->assign('bname', 'BenutzernameHans');
+	include("dbConnection.class.php");
+	
+	//Benutzerdaten auslesen
+	$sqlSelect = "SELECT * FROM benutzer WHERE bname = 'ms'";
+	$stmt = mysqli_query($conn, $sqlSelect);
+	$singleRow = mysqli_fetch_assoc($stmt);
+	
+	$smarty->assign('vorname', $singleRow['vorname']);
+	$smarty->assign('nachname', $singleRow['nachname']);
+	$smarty->assign('strasse', $singleRow['strasse']);
+	$smarty->assign('nummer', $singleRow['hausnummer']);
+	$smarty->assign('plz', $singleRow['plz']);
+	$smarty->assign('ort', $singleRow['ort']);
+	$smarty->assign('land', $singleRow['land']);
+	$smarty->assign('email', $singleRow['email']);
+	$smarty->assign('bbild', $singleRow['bbild']);
+	$smarty->assign('bname', $singleRow['bname']);
+	
+	//das Alter wird berechnet (aktuelles Jahr - Gebutrsjahr)
+	date_default_timezone_set("Europe/Berlin");
+	$timestamp = time();
+	$geburtsdatum = strtotime($singleRow['geburtsdatum']);
+	$alter = date ('Y', $timestamp) - date ('Y', $geburtsdatum);
+	$smarty->assign('alter', $alter);
+	
 	$smarty->display('meineDaten.tpl');
+	
+	mysqli_close($conn);
 }else{
 	header("Location: datenBearbeiten.class.php");
 }
