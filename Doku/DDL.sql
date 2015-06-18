9a77494eb002b732b46ea8d02fefc50123e9857e
a1306537 <a1306536@unet.univie.ac.at>
2015-06-18 19:24:41 +0200

    DDL

--- Doku/DDL.sql
+++ Doku/DDL.sql
@@ -67,9 +67,9 @@ CREATE TABLE bewertung (
 );
 
 -- Inserts:
-INSERT INTO benutzer VALUES('Max', 'max@hotmail.com', 'Max', 'Maximilian', 'Mustermann', '1992-06-09', 'JÃ¤gerstrasse', '123', 1130, 'Wien', 'Austria', 'm', 'Max.jpg');
+INSERT INTO benutzer VALUES('Max', 'max@hotmail.com', 'Max', 'Maximilian', 'Mustermann', '1992-06-09', 'Jägerstrasse', '123', 1130, 'Wien', 'Austria', 'm', 'Max.jpg');
 
-INSERT INTO kategorie VALUES ('FrÃ¼hstÃ¼ck', 'fruestueck.jpg', 'Lecker lecker Fruestueck');
+INSERT INTO kategorie VALUES ('Frühstück', 'fruestueck.jpg', 'Lecker lecker Fruestueck');
 INSERT INTO kategorie VALUES ('Vorspeise', 'vorspeise.jpg', 'Lecker lecker Vorspeise');
 INSERT INTO kategorie VALUES ('Hauptspeise', 'hauptspeise.jpg', 'Lecker lecker Hauptspeise');
 INSERT INTO kategorie VALUES ('Dessert', 'dessert.jpg', 'Lecker lecker Dessert');
@@ -78,21 +78,21 @@ INSERT INTO kategorie VALUES ('Salat', 'salat.jpg', 'Lecker lecker Salat');
 
 INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Schokoladenkuchen', SYSDATE(), 'Lecker Lecker Schoko', 'Schokoladenkuchen.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
 INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Bananenkuchen', SYSDATE(), 'Lecker Lecker Banane', 'Bananenkuchen.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
-INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'KÃ¤sekuchen', SYSDATE(), 'Lecker Lecker KÃ¤se', 'KÃ¤sekuchen.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
+INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Käsekuchen', SYSDATE(), 'Lecker Lecker Käse', 'Käsekuchen.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
 INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Zitronenkuchen', SYSDATE(), 'Lecker Lecker Zitrone', 'Zitronenkuchen.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
 INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Rhabarberkuchen', SYSDATE(), 'Lecker Lecker Rhabarber', 'Rhabarberkuchen.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
 INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Namibischer Bananenkuchen', SYSDATE(), 'Lecker Lecker Namibia', 'Namibischer Bananenkuchen.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
-INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Gerollte Mini-NusshÃ¶rnchen', SYSDATE(), 'Lecker Lecker HÃ¶rnchen', 'Gerollte Mini-NusshÃ¶rnchen.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
-INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'SchÃ¼ttelkuchen', SYSDATE(), 'Lecker Lecker SchÃ¼ttelkÃ¼chen', 'SchÃ¼ttelkuchen.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
+INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Gerollte Mini-Nusshörnchen', SYSDATE(), 'Lecker Lecker Hörnchen', 'Gerollte Mini-Nusshörnchen.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
+INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Schüttelkuchen', SYSDATE(), 'Lecker Lecker Schüttelküchen', 'Schüttelkuchen.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
 INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Mozart-Torte', SYSDATE(), 'Lecker Lecker Mozart', 'Mozart-Torte.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
-INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Biskuitrolle mit ErdbeerfÃ¼llung', SYSDATE(), 'Lecker Lecker Erdbeere', 'Biskuitrolle mit ErdbeerfÃ¼llung.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
-INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Japanische MelonenbrÃ¶tchen', SYSDATE(), 'Lecker Lecker Melone', 'Japanische MelonenbrÃ¶tchen.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
+INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Biskuitrolle mit Erdbeerfüllung', SYSDATE(), 'Lecker Lecker Erdbeere', 'Biskuitrolle mit Erdbeerfüllung.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
+INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Japanische Melonenbrötchen', SYSDATE(), 'Lecker Lecker Melone', 'Japanische Melonenbrötchen.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
 INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Schokoladenmuffins mit einem Marshmellowhut', SYSDATE(), 'Lecker Lecker Muffin', 'Schokoladenmuffins mit einem Marshmellowhut.jpg', 'Schoko, Mehl, Zucker', 1, 'Dessert');
 
-INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'FrÃ¼hstÃ¼cksmuffins ', SYSDATE(), 'Lecker Lecker FrÃ¼hstÃ¼cks-Muffin', 'FrÃ¼hstÃ¼cksmuffins.jpg', 'Schoko, Mehl, Zucker', 1, 'FrÃ¼hstÃ¼ck');
-INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'KÃ¼rbiscremesuppe ', SYSDATE(), 'Lecker Lecker KÃ¼rbisse', 'KÃ¼rbiscremesuppe.jpg', 'Schoko, Mehl, Zucker', 1, 'Vorspeise');
+INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Frühstücksmuffins ', SYSDATE(), 'Lecker Lecker Frühstücks-Muffin', 'Frühstücksmuffins.jpg', 'Schoko, Mehl, Zucker', 1, 'Frühstück');
+INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Kürbiscremesuppe ', SYSDATE(), 'Lecker Lecker Kürbisse', 'Kürbiscremesuppe.jpg', 'Schoko, Mehl, Zucker', 1, 'Vorspeise');
 INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Wiener Schnitzel', SYSDATE(), 'Lecker Lecker Schnitzel', 'Wiener Schnitzel.jpg', 'Schoko, Mehl, Zucker', 1, 'Hauptspeise');
-INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'ErdÃ¤pfelsalat ', SYSDATE(), 'Lecker Lecker erdige Ã„pfel', 'ErdÃ¤pfelsalat.jpg', 'Schoko, Mehl, Zucker', 1, 'Beilage');
+INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Erdäpfelsalat ', SYSDATE(), 'Lecker Lecker erdige Äpfel', 'Erdäpfelsalat.jpg', 'Schoko, Mehl, Zucker', 1, 'Beilage');
 INSERT INTO beitrag(bname, titel, bdatum, btext, bild, zutaten, portion, kname) VALUES ('Max', 'Steirischer Krautsalat', SYSDATE(), 'Lecker Lecker Steiermark', 'Steirischer Krautsalat.jpg', 'Schoko, Mehl, Zucker', 1, 'Salat');
 
 INSERT INTO kommentar (kdatum, ktext) VALUES (SYSDATE(), 'Schmeckt etwas langweilig!'); 
